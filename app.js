73a45ae3f8c012180ae212a36b5e0b802e74a303
RajabAliDowadji <142385077+RajabAliDowadji@users.noreply.github.com>
2025-01-27 23:43:22 +0530

    versel changes completed

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ const {
 // Constant Import End
 
 const app = express();
+
+app.get("/", (req, resp) => {
+  resp.status(200).send("Hello from RD STORE");
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -57,4 +62,5 @@ app.use(ROUTES.ROOT, superAdminAuthValidation, ProductCategoriesRoutes);
 app.use(ROUTES.ROOT, superAdminAuthValidation, ProductSubCategoriesRoutes);
 app.use(ROUTES.ROOT, superAdminAuthValidation, CommissionRoutes);
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+app.listen(port);
