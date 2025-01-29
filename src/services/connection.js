b813325800f39668ee802c997f05179642ba5bb2
RajabAliDowadji <142385077+RajabAliDowadji@users.noreply.github.com>
2025-01-29 20:27:57 +0530

    mongodb issue solved

--- src/services/connection.js
+++ src/services/connection.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const config = require("../config/default.json");
 require("dotenv").config({ path: process.env.MONGODB_URL });
 
-const mongodbURL = process.env.MONGODB_URL;
-console.log("Abdeali mongodbURL", mongodbURL);
-
 try {
-  mongoose.connect(mongodbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(
+    "mongodb+srv://rajabalidowadjistore:VMYJt1rCrMsy80dE@rdstore.ttkyf.mongodb.net/rdstore?retryWrites=true&w=majority&appName=rdstore",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 } catch (error) {
   console.log(error);
 }
